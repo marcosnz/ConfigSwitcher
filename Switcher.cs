211c5becf27089267ceb2eb02046d7dba0535699
Mark Walker <Mark@twotales.co.nz>
2015-05-28 16:46:12 +1200

    Skeleton done - all current files passing

---
+++ Switcher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigSwitcher
+{
+  public class Switcher
+  {
+    public enum TargetType
+    {
+      Dev,
+      UAT,
+      Prod,
+      Ignore
+    }
+    static void Main(string[] args)
+    {
+      var prog = new Switcher();
+      prog.Switch("AKDEV");
+    }
+
+    public TargetType Switch(string identifier)
+    {
+      if (identifier.ToUpperInvariant().Contains("DEV") || identifier.Contains("Demo"))
+      {
+        return TargetType.Dev;
+      }
+
+      if (identifier.Contains("UAT"))// || identifier.Contains("Demo"))
+      {
+        return TargetType.UAT;
+      }
+
+      if (identifier.StartsWith("Template"))
+      {
+        return TargetType.Ignore;
+      }
+
+      switch (identifier)
+      {
+        case "Test1":
+        case "Test2":
+        case "Test3":
+        case "feature":
+        case "trunk":
+          return TargetType.Dev;
+        default:
+          return TargetType.Prod;
+      }
+
+
+    }
+  }
+}
