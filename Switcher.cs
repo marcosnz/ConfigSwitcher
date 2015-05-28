20c90bab3a6031a85d18aca452c2e4ce4b774e93
Mark Walker <Mark@twotales.co.nz>
2015-05-28 18:13:43 +1200

    More robust checking. Add target to XML

--- Switcher.cs
+++ Switcher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConfigSwitcher
 {
@@ -13,44 +14,61 @@ namespace ConfigSwitcher
       Dev,
       UAT,
       Prod,
-      Ignore
+      Ignore,
+      Unknown
     }
     static void Main(string[] args)
     {
       var prog = new Switcher();
-      prog.Switch("AKDEV");
+      prog.GetTargetType("AKDEV");
     }
 
-    public TargetType Switch(string identifier)
+    public TargetType GetTargetType(string identifier)
     {
-      if (identifier.ToUpperInvariant().Contains("DEV") || identifier.Contains("Demo"))
-      {
-        return TargetType.Dev;
-      }
 
-      if (identifier.Contains("UAT"))// || identifier.Contains("Demo"))
-      {
-        return TargetType.UAT;
-      }
-
-      if (identifier.StartsWith("Template"))
+      if (identifier.Contains("\\Template"))
       {
         return TargetType.Ignore;
       }
 
-      switch (identifier)
+      if (identifier.Contains("\\AirAsia") || identifier.Contains("\\MT") || identifier.Contains("\\Multi") || identifier.ToLowerInvariant().Contains("\\prod_") || identifier.Contains("\\Production\\"))
       {
-        case "Test1":
-        case "Test2":
-        case "Test3":
-        case "feature":
-        case "trunk":
-          return TargetType.Dev;
-        default:
+        if (!identifier.Contains("UAT") && !identifier.Contains("DEV") && !identifier.Contains("Demo"))
+        {
           return TargetType.Prod;
+        }
       }
 
+      if (identifier.Contains("UAT"))
+      {
+        return TargetType.UAT;
+      }
 
+      if (identifier.Contains("\\Test\\Deployments") || identifier.ToUpperInvariant().Contains("DEV") || identifier.Contains("Demo"))
+      {
+        return TargetType.Dev;
+      }
+
+      return TargetType.Unknown;
+    }
+
+    public string AddTargetIfMissing(string current, string target)
+    {
+      var rootElement = XElement.Parse(current);
+      XNamespace ns = rootElement.GetDefaultNamespace();
+      //if (! element.Descendants("target")
+      //XNamespace ns = XNamespace.Get("http://schemas.microsoft.com/developer/msbuild/2003");
+      var groupElement = rootElement.Element(ns + "PropertyGroup");
+      if (groupElement != null)
+      {
+        groupElement.AddFirst(new XElement(ns + "target", target));
+      }
+      else
+      {
+        throw new Exception("PropertyGroup element not found");
+      }
+
+      return rootElement.ToString();
     }
   }
 }
